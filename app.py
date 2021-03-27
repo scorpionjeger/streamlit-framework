2018df6dfca5aebf221a48e065cf054bfcc79eff
Parrish Brady <scorpionjeger@hotmail.com>
2021-03-26 22:35:26 -0400

    again??????

--- app.py
+++ app.py
@@ -10,14 +10,8 @@ from bokeh.plotting import figure, output_file, show
 import datetime
 from bokeh.models import Label, Title, NumeralTickFormatter
 from dotenv import load_dotenv
-#from boto.s3.connection import S3Connection
 
-
-st.title('Month long Stock time slice')
-
-
-
-
+st.title('Month long Stock time slice \nby Parrish Brady')
 
 
 d = st.date_input(
@@ -27,42 +21,20 @@ st.write('Start Date is ', d)
 Ticker = st.text_input('Stock Ticker', 'IBM')
 st.write('The current Stock Ticker is ', Ticker)
 
-
-#Ticker="IBM"
-
-
 if 1:
     if 0:
         load_dotenv('.env')
         APIkey=os.getenv('api_token')
     if 1:
-        #APIkey = S3Connection(os.environ['APIkey'])
-        APIkey = os.environ['APIkey']
-    print(APIkey)
-    #APIkey="GJKSUSWKNT7GMC1N"
 
-    #r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey='+APIkey+'')
+        APIkey = os.environ['APIkey']
+
     r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+Ticker+"&outputsize=full&apikey="+APIkey)
 
-    #r = requests.get(
-    #    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo")
 
-    #print(r)
-    #print(r.status_code)
-
-    #print(r.headers['content-type'])
-
-    #print(r.encoding)
-
-    #print(r.text)
 
     gty=r.json()
-    #print(gty)
     r2=pd.DataFrame.from_dict(gty['Time Series (Daily)'])
-    #print(r2)
-    #print(r2.columns)
-
-    #print(datetime.date(int(r2.columns[0].split("-")[0]),int(r2.columns[0].split("-")[1]),int(r2.columns[0].split("-")[2])))
     StockDate=[]
     ClosePrice=[]
 
@@ -72,10 +44,8 @@ if 1:
         priceTime=datetime.date(int(price.split("-")[0]),int(price.split("-")[1]),int(price.split("-")[2]))
 
         if priceTime>=d and priceTime<d+thirtydays:
-                #StockDate.append(datetime.date(int(price.split("-")[0]),int(price.split("-")[1]),int(price.split("-")[2])))
                 StockDate.append(price)
                 ClosePrice.append(r2[price]['4. close'])
-                #print(r2[price]['4. close'])
 
 
 
@@ -90,13 +60,8 @@ if 1:
 
     if 1:
 
-        cc=alt.Chart(Rpd).mark_line().encode(
-            x='StockDate',
-            y='ClosePrice'
-        )
 
         c=alt.Chart(Rpd).mark_line().encode(
-#            alt.X('StockDate', axis=alt.Axis(format='$.2f')),
             alt.X('StockDate'),
             alt.Y('ClosePrice', axis=alt.Axis(format='$.2f'))
 
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"scorpionjeger@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
